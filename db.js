f353e22bf73a8bed81bc38b1df5df8095125a670
OliWongDev <14wongom@gmail.com>
2023-01-24 21:40:05 +1100

    2. Validate Email & Password

---
+++ db.js
@@ -0,0 +1,8 @@
+export const users = [
+  {
+    email: "laithharb@hotmail.com",
+    password: "123456789"
+  }
+]
+
+export default users
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,44 @@
 import { Router } from 'express'
+import { check, validationResult } from 'express-validator'
+import { users } from "../db.js"
+
 const router = Router()
 
-router.post('/signup', (req, res) => {
+router.post('/signup', [
+  check("email", "Please provide a valid email")
+    .isEmail(),
+  check("password", "Please provide a password that is greater than 5 characters.")
+    .isLength({
+      min: 6
+    })
+], (req, res) => {
   const { password, email } = req.body
 
-  console.log(password, email)
+  // VALIDATED THE INPUT
+  const errors = validationResult(req)
 
-  res.send("Auth route working")
+  if (!errors.isEmpty())
+    return res.status(400).json({
+      errors: errors.array()
+    })
+
+  // VALIDATE IF USER DOESN'T ALREADY EXIST
+
+  let user = users.find((user) => {
+    return user.email === email
+  })
+
+  if(user) {
+    res.status(400).json({
+      "errors": [
+        {
+          "msg": "This user already exists"
+        }
+      ]
+    })
+  }
+
+  res.send("Validation Pass")
 })
 
 export default router
